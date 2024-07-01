3f895145b95c857ee2b1fdf463da03d68eb808eb
gglas <gordonglas@gmail.com>
2024-07-01 07:36:50 -0500

    Convert tabs to 2 spaces, update astrojs

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -2,22 +2,22 @@ import MarkdownIt from 'markdown-it';
 import { getCollection } from 'astro:content';
 
 export async function getPublishedPostsByDateDesc() {
-    const isDev = import.meta.env.DEV;
+  const isDev = import.meta.env.DEV;
 
-    const posts = (await getCollection('blog'))
-    .filter(
-        // only published posts make it to production
-        (post) => post.data.published || isDev
-    ).sort(
-        // newest posts first
-        (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
-    );
+  const posts = (await getCollection('blog'))
+  .filter(
+    // only published posts make it to production
+    (post) => post.data.published || isDev
+  ).sort(
+    // newest posts first
+    (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
+  );
 
-    return posts;
+  return posts;
 }
 
 const stripHtml = (html: string) => {
-	return html.replace(/<\/?[^>]+(>|$)/g, '');
+  return html.replace(/<\/?[^>]+(>|$)/g, '');
 };
 
 // Removes leading "import" lines (allowed in mdx).
@@ -25,27 +25,27 @@ const stripHtml = (html: string) => {
 // but this can be adjusted to strip out more, if needed.
 // Alternatively, could try: https://stackoverflow.com/a/77537167
 const stripMdx = (mdx: string) => {
-	if (mdx.trim().startsWith('import ')) {
-		return '';
-	}
-	return mdx;
+  if (mdx.trim().startsWith('import ')) {
+    return '';
+  }
+  return mdx;
 };
 
 // Modified verison of:
 // https://www.paulie.dev/posts/2023/09/how-to-create-excerpts-with-astro/
 export function createExcerpt(body: string, maxChars: number) {
-	const parser = new MarkdownIt();
-	return parser
-		.render(body) // render markdown as html (Note: any mdx code still exists after render())
-		.split('\n')  // split into array of lines
-		.slice(0, 10) // take first 10 lines (might need more if post has lots of leading mdx)
-		.map((str: string) => {
-			let s = stripHtml(str);
-			s = stripMdx(s);
-			return s.split('\n'); // return array
-		})
-		.flat()		// concat arrays
-		.join(' ')	// concat into string
-		.substring(0, maxChars)	// take up to maxChars
-		.trim();	// trim whitespace
+  const parser = new MarkdownIt();
+  return parser
+    .render(body) // render markdown as html (Note: any mdx code still exists after render())
+    .split('\n')  // split into array of lines
+    .slice(0, 10) // take first 10 lines (might need more if post has lots of leading mdx)
+    .map((str: string) => {
+      let s = stripHtml(str);
+      s = stripMdx(s);
+      return s.split('\n'); // return array
+    })
+    .flat()     // concat arrays
+    .join(' ')  // concat into string
+    .substring(0, maxChars) // take up to maxChars
+    .trim();  // trim whitespace
 };
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,17 +3,17 @@ import {getPublishedPostsByDateDesc, createExcerpt} from '../lib/blog';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
-	const posts = await getPublishedPostsByDateDesc();
-	return rss({
-		title: `${SITE_TITLE} - blog`,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		customData: `<generator>AstroJS</generator>`,
-		trailingSlash: false,
-		items: posts.map((post) => ({
-			...post.data,
-			description: `${createExcerpt(post.body, 280)}...`,
-			link: `/blog/${post.slug}`,
-		})),
-	});
+  const posts = await getPublishedPostsByDateDesc();
+  return rss({
+    title: `${SITE_TITLE} - blog`,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    customData: `<generator>AstroJS</generator>`,
+    trailingSlash: false,
+    items: posts.map((post) => ({
+      ...post.data,
+      description: `${createExcerpt(post.body, 280)}...`,
+      link: `/blog/${post.slug}`,
+    })),
+  });
 }
