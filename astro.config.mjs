86dceaeb1a3eebcdb9d04bb93d6aff75acebd100
gglas <gordonglas@gmail.com>
2024-07-11 07:09:45 -0500

    Switch to use prism instead of shiki for syntax-highlighting of markdown code blocks, prism is built-in - so don't need our own prism.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+// https://docs.astro.build/en/reference/configuration-reference/#markdown-options
+// https://docs.astro.build/en/guides/markdown-content/#extending-markdown-config-from-mdx
 export default defineConfig({
   site: 'https://gglas.ninja',
   integrations: [mdx(), sitemap()],
+  markdown: {
+    syntaxHighlight: 'prism',
+  },
 });
